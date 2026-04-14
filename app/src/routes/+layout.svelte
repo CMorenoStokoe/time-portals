38d20116963191fc4f9e8af02069f82fc3ee48bb
Dr. Chris Moreno-Stokoe <48444574+CMorenoStokoe@users.noreply.github.com>
2026-04-14 12:01:17 +0100

    Added speed insights

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -7,6 +7,8 @@
 	// Add vercel analytics
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	injectSpeedInsights();
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
